50421eaad7d304e62e56e6124e470be5c1c20b91
majid Amini <majidamini_s@yahoo.com>
2022-05-27 14:23:10 +0430

    time function added to project

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const btn_Numbers = document.querySelectorAll('.btn--number');
 const btn_Opearations = document.querySelectorAll('.operation');
 const btn_Clear = document.querySelector('.clear');
 const btn_Delete = document.querySelector('.delete');
-
 const btn_execute = document.querySelector('.execute');
 
 const prevDisplay = document.querySelector('.display__prev');
 const currentDisplay = document.querySelector('.display__current');
+const time = document.querySelector('.time');
 
+
+
+//time-function
+const date = new Date();
+const hour = date.getHours();
+const minute = date.getMinutes();
+const second = date.getSeconds();
+
+time.innerHTML = `${hour} : ${minute}`;
 
 
 
@@ -60,5 +69,7 @@ btn_execute.addEventListener('click', () => {
     
     currentDisplay.textContent = eval(currentDisplay.textContent);
 });
+
+
 
 
